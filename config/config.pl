9965b7cead96a7312fe1cda9ad480ba59d56c544
Dan Gildea <dgildea>
2026-01-01 10:25:11 -0500

    Release v2026

--- config/config.pl
+++ config/config.pl
@@ -441,8 +441,8 @@ use L2hos;
 use vars qw(%prefs %cfg %newcfg);
 
 # This is the central place to modify the release name and date!!!
-my $RELEASE = '2025';
-my $VERSION = 'Released January 1, 2025';
+my $RELEASE = '2026';
+my $VERSION = 'Released January 1, 2026';
 
 # --------------------------------------------------------------------------
 # Open log
